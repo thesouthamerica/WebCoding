585750d76ba1d084c187a5ffc30361668f5e034e
Leonardo Bezerra <pxbezerraleonardo@gmail.com>
2024-11-15 01:06:53 -0300

    Add files via upload

---
+++ 17-exercicio javascript/js/script.js
@@ -0,0 +1,2 @@
+let cor = prompt("Digite uma cor para o fundo da página (exemplo: red, blue, green); Você pode usar valores Hexadecimal; Caso a cor seja invalida o fundo será definido como Branco,");
+document.body.style.backgroundColor = cor;
