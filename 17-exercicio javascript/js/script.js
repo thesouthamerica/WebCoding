7e8ea66d256d26eb1c070018d2783a4a7ccb14fb
Leonardo Bezerra <pxbezerraleonardo@gmail.com>
2024-11-15 14:39:21 -0300

    Add files via upload

--- 17-exercicio javascript/js/script.js
+++ 17-exercicio javascript/js/script.js
@@ -1,2 +1,6 @@
-let cor = prompt("Digite uma cor para o fundo da página (exemplo: red, blue, green); Você pode usar valores Hexadecimal; Caso a cor seja invalida o fundo será definido como Branco,");
+let cor = prompt("Digite uma cor para o fundo da página (exemplo: red, blue, green); Você pode usar valores Hexadecimal (exemplo: #369369); Caso a cor seja invalida o fundo será definido como Branco,");
 document.body.style.backgroundColor = cor;
+
+let nome = document.getElementById("nome")
+nome.textContent = 'Leonardo Peixoto Xavier Bezerra';
+nome.style.color = 'white';
